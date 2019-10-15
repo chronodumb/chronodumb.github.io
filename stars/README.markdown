e06e1c5d552abcd35aba8eb5ff9d537f6c5aed8e
homer <homer@sinonet.ph>
2019-10-15 17:09:27 +0800

    x

---
+++ stars/README.markdown
@@ -0,0 +1,6 @@
+# particles.js demo / Stars version
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/chasebank/pen/QjMaKq](https://codepen.io/chasebank/pen/QjMaKq).
+
+ Made with particles.js, a lightweight JavaScript library for creating particles
+
+Forked from [Johan](http://codepen.io/bob6664569/)'s Pen [particles.js demo / Stars version](http://codepen.io/bob6664569/pen/rOzmve/).
---
+++ stars/dist/script.js
@@ -0,0 +1,122 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 0,
+      "density": {
+        "enable": true,
+        "value_area": 500
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 1
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 0.2,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "size_min": 0,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": true,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 83.91608391608392,
+        "size": 1,
+        "duration": 3,
+        "opacity": 1,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 1
+      },
+      "remove": {
+        "particles_nb": 4
+      }
+    }
+  },
+  "retina_detect": true
+});
+var count_particles, stats, update; score = 0;
+count_particles = document.querySelector('.js-count-particles');
+
+update = function() {
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    _score = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);;
+
